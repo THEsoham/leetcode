aadef59ce855205136ea622108b8f88151a3af86
soham guha <97176121+THEsoham@users.noreply.github.com>
2026-09-05 16:25:05 +0530

    Time: 0 ms (100%), Space: 12.8 MB (66.58%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,9 +5,23 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
-        for i in range(len(nums)):
-            if nums[i] >= target:
-                return i
 
-        return len(nums)
+        left = 0
+        right = len(nums)-1
+
+        while left <= right:
+            mid = (left + right)//2
+
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] == target:
+                return mid
+        else:
+            return left
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
