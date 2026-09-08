d0ea9b4ba3d99da83e82d7f01a5322c323c9e239
soham guha <97176121+THEsoham@users.noreply.github.com>
2026-09-08 20:32:02 +0530

    Time: 16 ms (57.31%), Space: 14.6 MB (8.52%) - LeetHub - Time: O(n), Space: O(n)

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,8 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s1 = ''.join([char for char in s if char.isalnum()])
-        if s1.lower() == s1.lower()[::-1]:
+        l1 = []
+        for i in s:
+            if i.isalnum():
+                l1.append(i.lower())
+        if l1 == l1[::-1]:
             return True
         else:
             return False
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
