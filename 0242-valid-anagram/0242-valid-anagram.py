d2667b88d5d569e5d572b1eb6b4f15030f29f13f
soham guha <97176121+THEsoham@users.noreply.github.com>
2026-09-08 20:37:30 +0530

    Time: 25 ms (30.28%), Space: 13.4 MB (39.87%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s = sorted(s)
+        t = sorted(t)
+        if s == t:
+            return True
+        else:
+            return False
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
